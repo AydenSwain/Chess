4d4951c608295563ea517c85c8757730a53a0a02
Ayden Swain <ayswain03@gmail.com>
2025-01-23 13:45:15 -0700

    Override equals() and hashCode() Remove bad code from knight

--- shared/src/main/java/chess/KnightMovesCalculator.java
+++ shared/src/main/java/chess/KnightMovesCalculator.java
@@ -3,25 +3,4 @@ package chess;
 import java.util.ArrayList;
 
 public class KnightMovesCalculator {
-    public KnightMovesCalculator(ChessBoard board, ChessPosition myPosition, ChessGame.TeamColor teamColor, ChessPiece.PieceType pieceType) {
-        super(board, myPosition, teamColor, pieceType);
-    }
-
-    public ArrayList<ChessMove> calculateMoves() {
-        clearMoves();
-
-        // Search up
-        searchDirection(1, 0);
-
-        // Search down
-        searchDirection(-1, 0);
-
-        // Search left
-        searchDirection(0, -1);
-
-        // Search right
-        searchDirection(0, 1);
-
-        return getMoves();
-    }
 }
