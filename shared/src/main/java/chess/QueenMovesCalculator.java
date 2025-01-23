2b410ecee991e0b1f3dd39e371198c0c65c60056
Ayden Swain <ayswain03@gmail.com>
2025-01-23 11:57:10 -0700

    calculateMoves methods implemented for the queen and knight

--- shared/src/main/java/chess/QueenMovesCalculator.java
+++ shared/src/main/java/chess/QueenMovesCalculator.java
@@ -1,4 +1,40 @@
 package chess;
 
+import java.util.ArrayList;
+
 public class QueenMovesCalculator {
+    public QueenMovesCalculator(ChessBoard board, ChessPosition myPosition, ChessGame.TeamColor teamColor, ChessPiece.PieceType pieceType) {
+        super(board, myPosition, teamColor, pieceType);
+    }
+
+    public ArrayList<ChessMove> calculateMoves() {
+        clearMoves();
+
+        // Search top left
+        searchDirection(1, -1);
+
+        // Search top right
+        searchDirection(1, 1);
+
+        // Search bottom left
+        searchDirection(-1, -1);
+
+        // Search bottom right
+        searchDirection(-1, 1);
+
+        // Search up
+        searchDirection(1, 0);
+
+        // Search down
+        searchDirection(-1, 0);
+
+        // Search left
+        searchDirection(0, -1);
+
+        // Search right
+        searchDirection(0, 1);
+
+        return getMoves();
+    }
 }
+
