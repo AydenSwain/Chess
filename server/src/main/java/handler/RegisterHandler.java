51d294658ce5f2863f368dfe38f1f6c535eae935
Ayden Swain <ayswain03@gmail.com>
2025-02-27 12:19:53 -0700

    Handler class working for register

--- server/src/main/java/handler/RegisterHandler.java
+++ server/src/main/java/handler/RegisterHandler.java
@@ -7,7 +7,8 @@ import spark.Response;
 
 public class RegisterHandler extends Handler implements Route {
 
-    public Object handle(Request request, Response response) throws Exception {
-
+    public Object handle(Request req, Response res) throws Exception {
+        UserData userData = fromJson(req.body(), UserData.class);
+        return toJson(userData);
     }
 }
