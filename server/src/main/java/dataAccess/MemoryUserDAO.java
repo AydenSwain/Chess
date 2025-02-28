9129d43db41d8d12abaadafc3a398a7bb937fb0e
Ayden Swain <ayswain03@gmail.com>
2025-02-28 16:49:32 -0700

    Implement MemoryUserDAO

--- server/src/main/java/dataAccess/MemoryUserDAO.java
+++ server/src/main/java/dataAccess/MemoryUserDAO.java
@@ -1,4 +1,24 @@
 package dataAccess;
 
+import model.UserData;
+
+import java.util.HashMap;
+
 public class MemoryUserDAO implements UserDataAccess{
+    private final HashMap<String, UserData> users = new HashMap<>();
+
+    @Override
+    public UserData getUserByName(String username) {
+        return users.get(username);
+    }
+
+    @Override
+    public void addUser(UserData user) {
+        users.put(user.name(), user);
+    }
+
+    @Override
+    public void clearUsers() {
+        users.clear();
+    }
 }
