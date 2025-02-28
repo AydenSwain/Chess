72ae1c3efc4eee6f2bf3a2015b1b0733b49c3b00
Ayden Swain <ayswain03@gmail.com>
2025-02-28 16:53:12 -0700

    Implement MemoryAuthDAO

--- server/src/main/java/dataAccess/MemoryAuthDAO.java
+++ server/src/main/java/dataAccess/MemoryAuthDAO.java
@@ -1,4 +1,24 @@
 package dataAccess;
 
+import model.AuthData;
+
+import java.util.HashSet;
+
 public class MemoryAuthDAO implements AuthDataAccess{
+    private final HashSet<AuthData> auths = new HashSet<>();
+
+    @Override
+    public boolean containsAuth(AuthData auth) {
+        return auths.contains(auth);
+    }
+
+    @Override
+    public void addAuth(AuthData auth) {
+        auths.add(auth);
+    }
+
+    @Override
+    public void clearAuths() {
+        auths.clear();
+    }
 }
