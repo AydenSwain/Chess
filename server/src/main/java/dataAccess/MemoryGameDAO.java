78f930c30c63c4e47f1c1566442f100be4290fa0
Ayden Swain <ayswain03@gmail.com>
2025-02-28 19:49:45 -0700

    Implement MemoryGameDAO

--- server/src/main/java/dataAccess/MemoryGameDAO.java
+++ server/src/main/java/dataAccess/MemoryGameDAO.java
@@ -1,4 +1,30 @@
 package dataAccess;
 
-//public class MemoryGameDAO implements GameDataAccess{
-//}
+import model.GameData;
+
+import java.util.Collection;
+import java.util.HashMap;
+
+public class MemoryGameDAO implements GameDataAccess{
+    private final HashMap<Integer, GameData> games = new HashMap<>();
+
+    @Override
+    public Collection<GameData> listGames() {
+        return games.values();
+    }
+
+    @Override
+    public void addGame(GameData game) {
+        games.put(game.gameId(), game);
+    }
+
+    @Override
+    public void updateGame(int oldGameId, GameData newGameData) {
+        games.put(oldGameId, newGameData);
+    }
+
+    @Override
+    public void clearGames() {
+        games.clear();
+    }
+}
