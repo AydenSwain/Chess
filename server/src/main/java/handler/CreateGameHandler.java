6da3aa921251b1912106e0555818776a8cb59d53
Ayden Swain <ayswain03@gmail.com>
2025-03-23 00:24:10 -0600

    Remove GameID record

--- server/src/main/java/handler/CreateGameHandler.java
+++ server/src/main/java/handler/CreateGameHandler.java
@@ -1,7 +1,6 @@
 package handler;
 
 import model.GameData;
-import model.GameID;
 import service.CreateGameService;
 import spark.Route;
 import spark.Request;
@@ -16,12 +15,18 @@ public class CreateGameHandler extends Handler implements Route {
 
             GameData newGameData = new CreateGameService().createGame(authToken, oldGameData);
 
-            GameID newGameID = new GameID(newGameData.gameID());
-            return toJson(newGameID);
+            GameData idGameData = onlyGameID(newGameData);
+
+            return toJson(idGameData);
+
         } catch (Unauthorized e) {
             return errorMessage(res, 401, "unauthorized");
         } catch (Exception e) {
             return errorMessage(res, 500, e.getMessage());
         }
+    }
+
+    private GameData onlyGameID(GameData gameData) {
+        return new GameData(gameData.gameID(), null, null, null, null);
     }
 }
