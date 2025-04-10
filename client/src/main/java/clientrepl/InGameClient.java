b128ddc27bb076e46a04332f27d671bd4cbbb15f
Ayden Swain <ayswain03@gmail.com>
2025-04-10 16:17:09 -0600

    Implememt InGameClient.eval

--- client/src/main/java/clientrepl/InGameClient.java
+++ client/src/main/java/clientrepl/InGameClient.java
@@ -1,7 +1,10 @@
 package clientrepl;
 
+import HTTPFacade.ResponseException;
 import HTTPFacade.ServerFacade;
 import websocket.*;
+
+import java.util.Arrays;
 
 public class InGameClient implements Client {
     private final ServerFacade facade;
@@ -21,6 +24,49 @@ public class InGameClient implements Client {
 
     @Override
     public String eval(String input) {
-        return "";
+        try {
+            var tokens = input.toLowerCase().split(" ");
+            String command = (tokens.length > 0) ? tokens[0] : "help";
+            String[] params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch (command) {
+                case "help" -> help();
+                case "redraw" -> redraw();
+                case "leave" -> leave();
+                case "make_move" -> makeMove(params);
+                case "resign" -> resign();
+                case "highlight_moves" -> highlightMoves(params);
+                case "quit" -> "";
+                default -> "Type \"help\" for help!";
+            };
+        } catch (NumberFormatException e) {
+            return "Invalid number format:\n" + e.getMessage();
+        } catch (ResponseException e) {
+            return e.getMessage();
+        }
+    }
+
+    private String redraw() {
+    }
+
+    private String leave() {
+    }
+
+    private String makeMove(String[] params) {
+    }
+
+    private String resign() {
+    }
+
+    private String highlightMoves(String[] params) {
+    }
+
+    private String help() {
+        return """
+            "redraw" <- To redraw board
+            "leave" <- To leave game
+            "make_move <row_number> <col_letter> -> <row_number> <col_letter>" <- To move a piece
+            "resign" <- To formally give up
+            "highlight_moves <row_number> <col_letter>" <- To highlight the legal moves for a piece
+            "quit" <- To quit""";
     }
 }
